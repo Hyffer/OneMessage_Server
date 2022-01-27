8a06ea54ccd9bc1ae1b3aab31305c9d3f86d7fb5
Hyffer <gaohuyufan@qq.com>
2022-01-27 16:48:38 +0800

    initial commit

---
+++ src/main/java/xyz/hyffer/onemessage_server/OneMessageServerApplication.java
@@ -0,0 +1,13 @@
+package xyz.hyffer.onemessage_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneMessageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneMessageServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/hyffer/onemessage_server/OneMessageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.hyffer.onemessage_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneMessageServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
