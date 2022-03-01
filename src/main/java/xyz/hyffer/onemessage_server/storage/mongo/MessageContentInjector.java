c945feb4f21196cc66c4131046f1b76891189159
Hyffer <gaohuyufan@qq.com>
2022-03-01 22:32:36 +0800

    Update MessageContentInjector.java

--- src/main/java/xyz/hyffer/onemessage_server/storage/mongo/MessageContentInjector.java
+++ src/main/java/xyz/hyffer/onemessage_server/storage/mongo/MessageContentInjector.java
@@ -2,9 +2,11 @@ package xyz.hyffer.onemessage_server.storage.mongo;
 
 import com.mongodb.MongoException;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.mongodb.core.DocumentCallbackHandler;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
@@ -21,14 +23,20 @@ public class MessageContentInjector {
     @Resource
     private MongoTemplate mongoTemplate;
 
+    @Resource
+    private MappingMongoConverter mongoConverter;
+
     public void injectContent(List<Message> messages, int _CID, int first_MID, int last_MID) {
         Query query = new Query(Criteria.where("_id").gte(first_MID).lte(last_MID));
-        query.fields().exclude("segments.content._class");
         mongoTemplate.executeQuery(query, "msgcontent_" + _CID, new DocumentCallbackHandler() {
             int i = 0;
             @Override
             public void processDocument(Document document) throws MongoException, DataAccessException {
-                messages.get(i).setSegments((ArrayList<MessageSegment>) document.get("segments"));
+                ArrayList<MessageSegment> segments = new ArrayList<>();
+                for (Object segmentBson : ((List) document.get("segments"))) {
+                    segments.add(mongoConverter.read(MessageSegment.class, (Bson) segmentBson));
+                }
+                messages.get(i).setSegments(segments);
                 i++;
             }
         });
