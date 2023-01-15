57bdc05ac2daf22b6a081dc995458eedd05290f9
Hyffer <gaohuyufan@qq.com>
2023-01-15 21:34:31 +0800

    fix: cannot pass test when packaging

--- src/test/java/xyz/hyffer/onemessage_server/OneMessageServerApplicationTests.java
+++ src/test/java/xyz/hyffer/onemessage_server/OneMessageServerApplicationTests.java
@@ -2,8 +2,10 @@ package xyz.hyffer.onemessage_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
+@WebAppConfiguration
 class OneMessageServerApplicationTests {
 
     @Test
